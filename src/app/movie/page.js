c10f1ba41f4581c309c72259d0aea31ca85e3ada
cvs2023 <cvtheking464@gmail.com>
2023-06-12 17:29:44 +0530

    dynamic routing is done, api work is done

---
+++ src/app/movie/page.js
@@ -0,0 +1,37 @@
+import MovieCard from "@/app/components/MovieCard";
+import styles from "@/app/styles/common.module.css";
+
+const Movie = async () => {
+  // await new Promise((resolve) => setTimeout(resolve, 2000));
+  const url =
+    "https://netflix54.p.rapidapi.com/search/?query=stranger&offset=0&limit_titles=50&limit_suggestions=20&lang=en";
+
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "9317109e33mshf2c6d938e1d7ea1p1bb8d8jsnb6af9c22f768",
+      "X-RapidAPI-Host": "netflix54.p.rapidapi.com",
+    },
+  };
+
+  const res = await fetch(url, options);
+  const data = await res.json();
+  const main_data = data.titles;
+
+  return (
+    <>
+      <section className={styles.movieSection}>
+        <div className={styles.container}>
+          <h1>Series & Movie</h1>
+          <div className={styles.card_section}>
+            {main_data.map((curElem) => {
+              return <MovieCard key={curElem.id} {...curElem} />;
+            })}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Movie;
